1466de87af21d37e519803515ad019a4c9f13684
alf767443 <andreluisfranca98@gmail.com>
2023-04-26 16:25:53 +0100

    Try

--- src/getHTOP.py
+++ src/getHTOP.py
@@ -1,48 +1,42 @@
 #!/usr/bin/env python3
 
-from GlobalSets.Mongo import DataSource as Source, Clients, DataBases as db, Collections as col
-from GlobalSets.localSave import createFile
-from tcppinglib import tcpping
+from GlobalSets.Mongo import DataSource as Source, DataBases as db, Collections as col
 
 import rospy, bson, rosnode, rosgraph
-import psutil, os
+import psutil, os, json
 from datetime import datetime
-import re
-
-dataPath = {
-    'dataSource': Source.CeDRI_UGV, 
-    'dataBase'  : db.dataLake,
-    'collection': col.Processes
-}
+from ros_monitoring.msg import _process
 
 class getNodes:
     def __init__(self) -> None:
-        rospy.init_node('HTOP', anonymous=False)
+        rospy.init_node('robot_HTOP', anonymous=False)
+        pub = rospy.Publisher('pub_HTOP', _process, queue_size = 5)
+
         rate = rospy.Rate(1)
         while not rospy.is_shutdown():  
-            try:              
-                data = []
-                process = psutil.process_iter(attrs=['status', 'cpu_num', 'pid', 'memory_full_info', 'cpu_percent', 'username', 'name', 'num_threads', 'memory_percent'])
-                for proc in process:
-                    # print(proc.info)
-                    try:
-                        temp = proc.info
-                            
-                    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-                        pass
-                    else:
-                        data.append(temp)
-                _data = {
-                    'process': data, 
-                    'computer': {                    
-                        'cpu_perc': psutil.cpu_percent(percpu=True),
-                        'memory': psutil.virtual_memory(),
-                        'memory_swap': psutil.swap_memory()
-                    },
-                    'dateTime': datetime.now()
-                    }
-                createFile(dataPath=dataPath, content=_data) 
-
+            try:           
+                process = _process()
+                process.cpu_percent = 0.1
+                pub.publish(process)
+                # data = []
+                # process = psutil.process_iter(attrs=['status', 'cpu_num', 'pid', 'memory_full_info', 'cpu_percent', 'username', 'name', 'num_threads', 'memory_percent'])
+                # for proc in process:
+                #     try:
+                #         temp = proc.info
+                #     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                #         pass
+                #     else:
+                #         data.append(temp)
+                # _data = {
+                #     'process': data, 
+                #     'computer': {                    
+                #         'cpu_perc': psutil.cpu_percent(percpu=True),
+                #         'memory': psutil.virtual_memory(),
+                #         'memory_swap': psutil.swap_memory()
+                #     },
+                #     'dateTime': datetime.now()
+                #     }
+                # _data = json.
             except Exception as e:
                 print(e)
             for i in range(0,60): rate.sleep()

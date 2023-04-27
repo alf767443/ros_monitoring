6fffab63a80b2b95755983dc66f2d5015cd6c706
alf767443 <andreluisfranca98@gmail.com>
2023-04-27 10:11:03 +0100

    First adapt in getSignal

---
+++ pingList.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+#============ Connection with ============#
+IP2PING = [
+    {'name':        'Remote Unit: Database',
+     'ip':          '192.168.217.183',
+     'port':        22, 
+     'interval':    1,
+     'timeout':     2, 
+     'count':       5,}]

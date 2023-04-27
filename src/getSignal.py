c7653c047b9694621cab6db731406851a435afdd
alf767443 <andreluisfranca98@gmail.com>
2023-04-27 15:29:06 +0100

    Comment and log the code

--- src/getSignal.py
+++ src/getSignal.py
@@ -9,57 +9,84 @@ from pingList import IP2PING
 
 class getSignal:
     def __init__(self) -> None:
-        # Inicia o nó
+        # Start the node
         rospy.init_node('getConnectionStatus', anonymous=False)
-        # Cria o publisher das mensagens
-        self.message_pub = rospy.Publisher("connectionStatus", SignalInformation, queue_size=10)
-        # Define a 
-        self.loop = asyncio.get_event_loop()
-        self.ip_list = IP2PING.copy()  # Armazena a lista de IPs a serem pingados
-        self.msg_list = [{}]
+
+        # Creates the publisher of the messages
+        try:
+            self.message_pub = rospy.Publisher("connectionStatus", SignalInformation, queue_size=10)
+        except Exception as e:
+            rospy.logerr("Failure to create publisher")
+            rospy.logerr("An exception occurred:", type(e).__name__,e.args)
+
+        # Set the asynchronous loop
+        try:
+            self.loop = asyncio.get_event_loop()
+        except:
+            rospy.logerr("Failure to asynchronous loop")
+            rospy.logerr("An exception occurred:", type(e).__name__,e.args)
         
-
+        # Create the global lists
+        try: 
+            # Copy the list of ips and settings defined in the pingList file
+            self.ip_list = IP2PING.copy()
+            # Creates an empty list to store the messages
+            self.msg_list = [{}]
+            # Create an empty list for asynchronous tasks
+            self.ping_tasks = {}
+            # Add verification keys to the lists of ips and messages
+            for i in range(0,len(self.ip_list)):
+                # Add an ID key in the list of ips and configurations
+                self.ip_list[i].update({'_id': i})
+                # Links the same key to the list of messages
+                self.msg_list.append({'_id': i})
+                rospy.loginfo("Ping to: " + self.ip_list[i]['ip'] + ':' + str(self.ip_list[i]['port']))
+                
+        except:
+            rospy.logerr("Failure to create the global lists")
+            rospy.logerr("An exception occurred:", type(e).__name__,e.args)
         
+        # Launches the ping_ips task asynchronously
+        try:
+            asyncio.run(self.ping_ips(self.ip_list))
+        except:
+            rospy.logerr("Error on launch task asynchronously")
+            rospy.logerr("An exception occurred:", type(e).__name__,e.args)
 
-        for i in range(0,len(self.ip_list)):
-            self.ip_list[i].update({'_id': i})
-            self.msg_list.append({'_id': i})
-            print(self.ip_list[i])
-
-        self.ping_tasks = {}  # Dicionário que mapeia os IPs para as tarefas de ping
-
-        print(1)
-
-        asyncio.run(self.ping_ips(self.ip_list))  # Inicia a tarefa assíncrona para pingar os IPs
-        
-        print(2)
+        # Keeps the no alive
         rospy.spin()
 
+# Function to ping against defined ips
     async def ping(self, ip_dict: dict):
         try:
-            print(3)
+            # Ping against ip_dict settings asynchronously
             aping = await async_tcpping(
                 address=ip_dict['ip'], 
                 port=ip_dict['port'],
                 timeout=ip_dict['timeout'],
                 count=ip_dict['count'],
                 interval=ip_dict['interval'])
-            print(4)
+            # Adds the ROS Info_ping message to the message list for the ip
             self.msg_list[ip_dict['_id']].update({'msg': self.ping2msg(ping=aping, publisher=self.message_pub)})
-            print(55)
-            await asyncio.sleep(delay=ip_dict['interval'])
-            print(555)
+            # Publishes the updated message to the ROS publisher
             await self.message_publish()
+            # Wait one more interval cycle to release finish the function
+            await asyncio.sleep(delay=ip_dict['interval'])
+            # Add to ip_list the dictionary that was removed
             self.ip_list.append(ip_dict)
         except Exception as e:
-            print(e)
+            rospy.logerr("Error on ping to " + ip_dict['ip'] + ':' + str(ip_dict['port']))
+            rospy.logerr("An exception occurred:", type(e).__name__,e.args)
         finally:
-            del self.ping_tasks[ip_dict['_id']]  # Remove a tarefa do dicionário de tarefas de ping
+            # Deletes the task from the task list
+            del self.ping_tasks[ip_dict['_id']]
 
-    def ping2msg(self, ping: models.TCPHost, publisher: rospy.Publisher):
+# Converts the ping values to the ROS message Info_ping
+    def ping2msg(self, ping: models.TCPHost):
         try:
-            print(6)
+            # Starts the message
             _msg = Info_ping()
+            # Fill in the message
             _msg.is_alive = int(ping.is_alive)
             _msg.packets_sent = int(ping.packets_sent)
             _msg.packets_loss = int(ping.packet_loss)
@@ -69,47 +96,62 @@ class getSignal:
             _msg.rtt_min = ping.min_rtt
             _msg.rtt_avg = ping.avg_rtt
             _msg.ip_target = ping.ip_address
-            print(7)
+            # Returns the converted message
             return _msg
         except Exception as e:
-            print(e)
+            rospy.logerr("Error on convert ping to message: " + ping.ip_address + ':' + ping.port)
+            rospy.logerr("An exception occurred:", type(e).__name__,e.args)
 
+# Publishes the ROS SignalInfomation message to the defined topic via the publisher
     async def publish(self):
-        print(12)
-        _msg = SignalInformation()
-        print(120)
-        _msg.ping
-        print(1200)
-        msg = []
-        print(12000)
         try:
-            for i in self.msg_list:
-                print(121)
-                msg.append(i['msg'])
-                print(i)
+            # Initiates the SignalInformation message 
+            _msg = SignalInformation()
+            # Start an empty list of Info_ping messages
+            msg = []
+            # Fills the msg list with the messages in the list self.msg_list
+            for item in self.msg_list:
+                # Adds the message to the _msg list
+                msg.append(item['msg'])
+            # Adds the message list to the ping field in the ROS message
+            _msg.ping = msg
         except Exception as e:
-            print(e)
-        print(12021)
-        _msg.ping = msg
-        print(120210)
-        self.message_pub.publish(_msg)
-        print(120212)
-
-
-    async def ping_ips(self, ip_list):
-        while not rospy.is_shutdown():  # Executa enquanto o roscore estiver ativo
-            for ip in self.ip_list:
-                print(ip)
-                if ip['_id'] not in self.ping_tasks:
-                    print(8)
-                    self.ping_tasks[ip['_id']] = asyncio.ensure_future(self.ping(ip_dict=ip))
-                    self.ip_list.remove(ip)
-                    print(9)
-                print(99)
-            await asyncio.sleep(0.1)
+            rospy.logerr("Error on convert ROS message")
+            rospy.logerr("An exception occurred:", type(e).__name__,e.args)
         
+        # Publishes the message to the publisher
+        try:    
+            self.message_pub.publish(_msg)
+        except Exception as e:
+            rospy.logerr("Error on publish the message")
+            rospy.logerr("An exception occurred:", type(e).__name__,e.args)
+        
+
+# Performs a read of the listed ips
+    async def ping_ips(self, ip_list):
+        # Keeps the loop going as long as ROS core is running
+        while not rospy.is_shutdown():
+            # For all items of ip_list
+            for item in self.ip_list:
+                try:
+                    # If no ping task exists for the _id of item create a task
+                    if item['_id'] not in self.ping_tasks:
+                        # Create a ping job for the item
+                        self.ping_tasks[item['_id']] = asyncio.ensure_future(self.ping(ip_dict=item))
+                        # Remove item from ip_list to decrease operations
+                        self.ip_list.remove(item)
+                except Exception as e:
+                    rospy.logerr("Error create the ping task to " + item['ip'] + ':' + str(item['port']))
+                    rospy.logerr("An exception occurred:", type(e).__name__,e.args)
+                
+            # Wait 0.1 s to start a new round of forcing so as not to overload processing
+            await asyncio.sleep(0.1)
+
+# On program interruption terminates asynchronous tasks
     def __del__(self):
         self.loop.stop()
+        rospy.rospy.loginfo("Interrupted asynchronous tasks")
+        
 
 if __name__ == '__main__':
     try:

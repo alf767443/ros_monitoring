351efead0a7464e972d55619fbcbe539fd596f96
alf767443 <andreluisfranca98@gmail.com>
2023-06-07 09:59:43 +0100

    Remove 127 from ping list

--- src/pingList.py
+++ src/pingList.py
@@ -3,14 +3,10 @@
 
 #============ Connection with ============#
 IP2PING = [
+    # Connection with the MongoDB server
     {'ip':          '192.168.217.183',
-     'port':        22, 
+     'port':        27017, 
      'interval':    1,
      'timeout':     1, 
      'count':       5,},
-
-    {'ip':          '127.0.0.1',
-     'port':        22, 
-     'interval':    1,
-     'timeout':     2, 
-     'count':       5,}]
+    ]
